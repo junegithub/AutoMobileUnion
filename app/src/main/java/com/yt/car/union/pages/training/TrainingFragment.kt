6253248ce00c8b95acc9c2ccaab94423c2c8a665
liupeilong <liupeilong@xiaomi.com>
2026-02-23 20:57:46 +0800

    培训首页布局â

--- app/src/main/java/com/yt/car/union/pages/training/TrainingFragment.kt
+++ app/src/main/java/com/yt/car/union/pages/training/TrainingFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.yt.car.union.databinding.FragmentTrainingBinding
+import com.yt.car.union.util.PressEffectUtils
 
 class TrainingFragment : Fragment() {
 
@@ -18,7 +19,29 @@ class TrainingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentTrainingBinding.inflate(inflater, container, false)
+        initView()
         return binding.root
+    }
+
+    private fun initView() {
+        PressEffectUtils.setCommonPressEffect(binding.trainingHomeRing)
+        PressEffectUtils.setCommonPressEffect(binding.trainingHomeUser)
+        PressEffectUtils.setCommonPressEffect(binding.trainingHomeLearn)
+
+        PressEffectUtils.setCommonPressEffect(binding.trainingHomeExamPast)
+        PressEffectUtils.setCommonPressEffect(binding.trainingHomeExamMeeting)
+        PressEffectUtils.setCommonPressEffect(binding.trainingHomeExamContinueEdu)
+        PressEffectUtils.setCommonPressEffect(binding.trainingHomeExamQualifyPast)
+
+        PressEffectUtils.setCommonPressEffect(binding.trainingHomePreTrainingTest)
+        PressEffectUtils.setCommonPressEffect(binding.trainingHomePreTrainingLearn)
+
+        PressEffectUtils.setCommonPressEffect(binding.trainingHomeDrivingLog)
+        PressEffectUtils.setCommonPressEffect(binding.trainingHomeSafetyCheck)
+        PressEffectUtils.setCommonPressEffect(binding.trainingHomeInspect)
+        PressEffectUtils.setCommonPressEffect(binding.trainingHomeDuty)
+
+        PressEffectUtils.setCommonPressEffect(binding.trainingHomeFindJob)
     }
 
     override fun onDestroyView() {
