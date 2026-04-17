bd892a7d8a30d955634724764bb77fd6d71b1da8
liupeilong <liupeilong@xiaomi.com>
2026-04-17 18:02:41 +0800

    Remove nested iOS project

--- ios-app/Bridge/WebViewBridge.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import WebKit
-
-final class WebViewBridge: NSObject, WKScriptMessageHandler {
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        // Placeholder for the future H5/native bridge.
-    }
-}
--- ios-app/Models/CommonResponse.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-struct CommonResponse<T: Decodable>: Decodable {
-    let code: Int
-    let message: String
-    let data: T?
-}
